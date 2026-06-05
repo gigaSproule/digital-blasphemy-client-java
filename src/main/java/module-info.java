4ca77b4e62839048060707128aaf1301983b3c0c
Benjamin <dev@benjaminsproule.com>
2026-06-05 21:12:28 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.benjaminsproule.digitalblasphemy.client {
+    exports com.benjaminsproule.digitalblasphemy.client;
+    exports com.benjaminsproule.digitalblasphemy.client.model;
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires static com.github.spotbugs.annotations;
+    requires java.net.http;
+}
